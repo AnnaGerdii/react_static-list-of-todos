5ea6cbc92a4daf7c3cb70921b6a31a60cb5f994c
Anna Gerdii <annagerdiy@gmail.com>
2019-12-03 15:33:21 +0200

    Updates

---
+++ precache-manifest.43d53ec96c5c924f7a83722fde2a5436.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "682147d0cb72974b28c21b51b521dd83",
+    "url": "/react_static-list-of-todos/index.html"
+  },
+  {
+    "revision": "d1bf606ea68b30240b50",
+    "url": "/react_static-list-of-todos/static/js/2.a7975bab.chunk.js"
+  },
+  {
+    "revision": "2dc3488fb17c0dd73c5c",
+    "url": "/react_static-list-of-todos/static/js/main.42ff1315.chunk.js"
+  },
+  {
+    "revision": "ea6f31786c2aed313835",
+    "url": "/react_static-list-of-todos/static/js/runtime~main.00529491.js"
+  }
+]);
