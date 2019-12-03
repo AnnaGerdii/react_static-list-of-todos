55b3b31d8072a2ab2f22966cc905fda51d90e0ec
Anna Gerdii <annagerdiy@gmail.com>
2019-12-03 18:54:17 +0200

    Updates

--- precache-manifest.1b024a041ed3590f3fca43e74ff30763.js
+++ precache-manifest.1b024a041ed3590f3fca43e74ff30763.js
@@ -1,15 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "682147d0cb72974b28c21b51b521dd83",
+    "revision": "344b1772258a7cca484b05a1239606f0",
     "url": "/react_static-list-of-todos/index.html"
+  },
+  {
+    "revision": "77220a171fd62d8e142e",
+    "url": "/react_static-list-of-todos/static/css/main.1dabf81c.chunk.css"
   },
   {
     "revision": "d1bf606ea68b30240b50",
     "url": "/react_static-list-of-todos/static/js/2.a7975bab.chunk.js"
   },
   {
-    "revision": "2dc3488fb17c0dd73c5c",
-    "url": "/react_static-list-of-todos/static/js/main.42ff1315.chunk.js"
+    "revision": "77220a171fd62d8e142e",
+    "url": "/react_static-list-of-todos/static/js/main.d44bc235.chunk.js"
   },
   {
     "revision": "ea6f31786c2aed313835",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react_static-list-of-todos/precache-manifest.43d53ec96c5c924f7a83722fde2a5436.js"
+  "/react_static-list-of-todos/precache-manifest.1b024a041ed3590f3fca43e74ff30763.js"
 );
 
 self.addEventListener('message', (event) => {
